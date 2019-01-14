7bd5af0008a58612b6eca7fbdc8371e25e82b1a9
eshan <eshanwp@gmail.com>
2019-01-14 16:09:10 +0530

    function

--- src/app/service/service-initializer/service-function-block/service-function-block.component.ts
+++ src/app/service/service-initializer/service-function-block/service-function-block.component.ts
@@ -11,6 +11,10 @@ export class ServiceFunctionBlockComponent implements OnInit {
   @Output() functionBlockJson = new EventEmitter<any>();
   jsonSchemaForm;
   dataObject: any;
+  convert: any;
+  className: any;
+  functionName: any;
+
   constructor(
     private serviceInitializerComponent: ServiceInitializerComponent
   ) { }
@@ -18,9 +22,20 @@ export class ServiceFunctionBlockComponent implements OnInit {
   ngOnInit() {
     this.serviceInitializerComponent.readJson('/function-block.json').then(res => {
       this.jsonSchemaForm = res;
+      console.log(this.jsonSchemaForm.schema.properties['class'].enum);
     }).catch(error => {
       console.log('functions bloc json getting error : ' + JSON.stringify(error, null, 2));
     });
+
+    // Read class_name.json file
+    this.serviceInitializerComponent.readJson('/class_name.json').then(res => {
+      this.dataObject = res;
+      console.log(this.jsonSchemaForm.schema.properties['class'].enum);
+      this.jsonSchemaForm.schema.properties['class'].enum = this.dataObject.enum;
+    }).catch(error => {
+      console.log('functions bloc json getting error : ' + JSON.stringify(error, null, 2));
+    });
+
   }
 
   addToFlow($event) {
@@ -28,4 +43,8 @@ export class ServiceFunctionBlockComponent implements OnInit {
     // this.functionBlockJson.emit(this.dataObject);
   }
 
+  getClass($event) {
+    this.className = $event.schema === undefined ? 'empty class' : $event.schema['class'];
+
+  }
 }
